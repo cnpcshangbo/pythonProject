d4abb59ab21e0d4bbb4ed8f9e6e41b6607660726
Bo Shang <cnpcshangbo@gmail.com>
2021-12-25 11:41:12 -0600

    Use a separate thread for reading line from Pipe.

--- launcher.py
+++ launcher.py
@@ -1,23 +1,43 @@
 import subprocess
+import logging
+import time
+import threading
 
-p = subprocess.Popen(["python", "1st.py"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
-# get output from process "Something to print"
-one_line_output = p.stdout.readline()
-print(one_line_output)
-one_line_output = p.stdout.readline()
-print(one_line_output)
+def thread_function(p, name):
+    logging.info("Thread %s: starting", name)
+    # get output from process "Something to print"
+    while True:
+        time.sleep(1)
+        one_line_output = p.stdout.readline()
+        logging.info("Thread %s: running", name)
+        logging.info(one_line_output)
+        if one_line_output == b"":
+            logging.info("Thread %s: finishing", name)
+            break
 
-# write 'a line\n' to the process
-p.stdin.write('a line\n'.encode())
-p.stdin.flush() # not necessary in this case
-# get output from process "not time to break"
-one_line_output = p.stdout.readline()
-print(one_line_output)
-# write "n\n" to that process for if r=='n':
-p.stdin.write('n\n'.encode())
-p.stdin.flush() # not necessary in this case
 
-# read the last output from the process  "Exiting"
-one_line_output = p.stdout.read()
-print(one_line_output)
+if __name__ == "__main__":
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+
+    p = subprocess.Popen(["python", "1st.py"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    x = threading.Thread(target=thread_function, args=(p, 1,))
+    x.start()
+    time.sleep(2)
+    # write 'a line\n' to the process
+    p.stdin.write('a line\n'.encode())
+    p.stdin.flush()  # not necessary in this case
+    # get output from process "not time to break"
+    # one_line_output = p.stdout.readline()
+    # print(one_line_output)
+
+    time.sleep(2)
+    # write "n\n" to that process for if r=='n':
+    p.stdin.write('n\n'.encode())
+    p.stdin.flush()  # not necessary in this case
+
+    # read the last output from the process  "Exiting"
+    # one_line_output = p.stdout.read()
+    # print(one_line_output)
--- mySubprocess.py
+++ mySubprocess.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+import logging
 import subprocess
 import sys
+import threading
+import time
 
 from PySide6.QtWidgets import QApplication, QWidget
 
@@ -32,11 +35,49 @@ class MainWindow(QWidget):
         # print(stdout)
         # print(stderr)
 
-        stdout = subprocess.check_output(' '.join(['sh -c', '\"source ~/ros_noetic_bare_bone_catkin_ws/install_isolated/setup.sh &&', self.ui.lineEdit.text(), "\""]), shell=True)
-        self.ui.textEdit.setText(stdout.decode("utf-8"))
-        self.ui.textEdit.verticalScrollBar().setValue(self.ui.textEdit.verticalScrollBar().maximum())
+        # stdout = subprocess.check_output(' '.join(['sh -c', '\"source ~/ros_noetic_bare_bone_catkin_ws/install_isolated/setup.sh &&', self.ui.lineEdit.text(), "\""]), shell=True)
+
+        format = "%(asctime)s: %(message)s"
+        logging.basicConfig(format=format, level=logging.INFO,
+                            datefmt="%H:%M:%S")
+        #if self.p is None:
+        # try:
+        #     self.p
+        # except NameError:
+        if not hasattr(self, 'p'):
+            logging.info("Creating Pipe.")
+            self.p = subprocess.Popen(["python", "1st.py"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+            x = threading.Thread(target=self.thread_function, args=(1,))
+            x.start()
+
+        self.p.stdin.write(self.ui.lineEdit.text().encode())
+        self.p.stdin.flush()  # not necessary in this case
+        # get output from process "not time to break"
+        # one_line_output = p.stdout.readline()
+        # print(one_line_output)
+
+        time.sleep(2)
+        # write "n\n" to that process for if r=='n':
+        self.p.stdin.write(self.ui.lineEdit.text().encode())
+        self.p.stdin.flush()  # not necessary in this case
+
+        # self.ui.textEdit.setText(stdout.decode("utf-8"))
+        # self.ui.textEdit.verticalScrollBar().setValue(self.ui.textEdit.verticalScrollBar().maximum())
 
 # self.ui.label_3.setText("test")
+
+    def thread_function(self, name):
+        logging.info("Thread %s: starting", name)
+        # get output from process "Something to print"
+        while True:
+            time.sleep(1)
+            one_line_output = self.p.stdout.readline()
+            logging.info("Thread %s: running", name)
+            logging.info(one_line_output)
+            if one_line_output == b"":
+                logging.info("Thread %s: finishing", name)
+                break
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
